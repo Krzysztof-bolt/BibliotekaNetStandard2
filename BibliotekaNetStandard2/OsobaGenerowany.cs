be6991a4c58e0d29594336a5826f03608775528e
Krzysztof Wołowski <78325693+Krzysztof-bolt@users.noreply.github.com>
2024-05-21 14:58:54 +0200

    indeksery/metody/operator

--- BibliotekaNetStandard2/OsobaGenerowany.cs
+++ BibliotekaNetStandard2/OsobaGenerowany.cs
@@ -46,4 +46,98 @@ public partial class Osoba
         }
     }
 
+    //indeksery
+    public Osoba this[int indeks]
+    {
+        get
+        {
+            return Dzieci[indeks];
+        }
+        set
+        {
+            Dzieci[indeks] = value;
+        }
+    }
+
+    public Osoba this[string nazwisko] 
+    {
+        get
+        {
+            return Dzieci.Find(o => o.Nazwisko == nazwisko);
+        }
+        set
+        {
+            Osoba znalezione = Dzieci.Find(p => p.Nazwisko == nazwisko);
+            if (znalezione is not null) znalezione = value;
+        }
+    }
+
+    //Implementownie dzialan w metodzie
+
+    private bool poSlubie = false;
+    public bool PoSlubie => poSlubie;
+
+    private Osoba? malzonek = null;
+    public Osoba? Malzonek => malzonek;
+
+    //statyczna metoda zawarcia malzenstwa
+
+    public static void Poslub(Osoba o1, Osoba o2)
+    {
+        o1.Poslub(o2);
+    }
+
+    //metoda zawarcia malzenstwa dla obiektu
+
+    public void Poslub(Osoba partner) 
+    {
+        if (poSlubie) return;
+        malzonek = partner;
+        poSlubie = true;
+        partner.Poslub(this); //this oznacza aktualny obiekt
+    }
+
+    //statyczna metoda "rozmnazania"
+    public static Osoba Prokreacja(Osoba o1, Osoba o2)
+    {
+        /*if(o1.Malzonek != o2)
+        {
+            throw new ArgumentException("Tylko malzonkowie moga sie rozmanzac");
+        }*/
+
+        Osoba dziecko = new()
+        {
+            Nazwisko = $"Dziecko osob {o1.Nazwisko} i {o2.Nazwisko}",
+            DataUrodzenia = DateTime.Now,
+        };
+
+        o1.Dzieci.Add(dziecko);
+        o2.Dzieci.Add(dziecko);
+
+        return dziecko;
+    }
+
+    //rozmanzanie metoda obiektu
+    public Osoba ProkreacjaZ(Osoba partner)
+    {
+        return Prokreacja(this, partner);
+    }
+
+    //opertaor zaslubin
+
+    public static bool operator +(Osoba o1 , Osoba o2)
+    {
+        Poslub(o1 , o2);
+        return o1.PoSlubie && o2.PoSlubie; // potwoerdz aze dwie osoby sa zaslubione
+    }
+
+    //opretor rozmanzania
+
+    public static Osoba operator *(Osoba o1, Osoba o2)
+    {
+        return Osoba.Prokreacja(o1 , o2);
+    }
+
+
+
 }
